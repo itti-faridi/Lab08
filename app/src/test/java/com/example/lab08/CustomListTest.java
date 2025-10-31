0d779496232d3842d2e8d3c60b43f5fcd6a993d2
Iteqaan <iteqaan.faridi@gmail.com>
2025-10-31 15:49:11 -0600

    hascity not implemented yet

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -2,6 +2,20 @@ package com.example.lab08;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+
 public class CustomListTest {
 
+    @Test
+    public void hasCity_returnsTrue_whenCityPresent() {
+        CustomList list = new CustomList();
+        City calgary = new City("Calgary", "AB");
+        list.addCity(calgary);
+        assertTrue(list.hasCity(calgary)); // should FAIL until hasCity is implemented
+    }
+
+    @Test
+    public void hasCity_returnsFalse_whenCityAbsent() {
+        CustomList list = new CustomList();
+        assertFalse(list.hasCity(new City("Edmonton", "AB")));
+    }
 }
